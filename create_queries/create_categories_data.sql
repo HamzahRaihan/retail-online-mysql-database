9525c1674df2161c21a476320d24944be8d5ba7f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-08 10:11:32 +0700

    create category

---
+++ create_queries/create_categories_data.sql
@@ -0,0 +1,17 @@
+INSERT INTO categories (category)
+VALUES
+('CPU'),
+('GPU'),
+('Motherboard'),
+('RAM'),
+('Storage'),
+('Power Supply'),
+('Computer Case'),
+('Monitor'),
+('Keyboard'),
+('Mouse'),
+('Headphones'),
+('Speakers');
+
+DESC categories;
+SELECT * FROM categories;
