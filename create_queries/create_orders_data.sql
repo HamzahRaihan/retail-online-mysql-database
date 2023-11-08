ed42df529da7d5422e7d095c6d2e982fab10a700
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-08 10:12:17 +0700

    create order data

---
+++ create_queries/create_orders_data.sql
@@ -0,0 +1,14 @@
+INSERT INTO orders (product_id, customer_id, quantity) VALUES
+(1, 1, 4),
+(2, 1, 2),
+(3, 1, 1),
+(4, 2, 3),
+(7, 3, 5),
+(9, 2, 2),
+(10, 4, 1),
+(9, 4, 1),
+(4, 4, 1),
+(1, 2, 1);
+
+DESC orders;
+SELECT * FROM orders;
