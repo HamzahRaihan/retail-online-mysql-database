700e43ef50d84a947afacb63b18d52a35f606e3f
hamzah raihan <101212035+HamzahRaihan@users.noreply.github.com>
2023-11-09 11:45:09 +0700

    Refactor select_average_transaction_monthly.sql

--- select_queries/select_average_transaction_monthly.sql
+++ select_queries/select_average_transaction_monthly.sql
@@ -1,3 +1,4 @@
+-- Active: 1699344612549@@127.0.0.1@3306@retail_online
 SELECT c.name, p.price AS price, o.quantity AS qty, o.orders_date AS date, ROUND(SUM(p.price * o.quantity)) AS total_transaction,
 ROUND(AVG(p.price)) as average
 FROM products p
@@ -5,8 +6,3 @@ JOIN orders o ON p.product_id = o.product_id
 JOIN customers c ON c.customer_id = o.customer_id
 WHERE o.orders_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH)
 GROUP BY c.name, price, qty, date;
-
-SELECT customer_id AS id, customers.name, ROUND(SUM(price)) AS total, ROUND(AVG(price)) AS average FROM orders
-JOIN customers ON (customers.id = orders.customer_id)
-WHERE MONTH(order_date) = 11
-GROUP BY id;
